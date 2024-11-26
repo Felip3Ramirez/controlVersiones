46910ed700e59095b64ad4758607ad298827d50e
Thomas <florezestiven4gmail.com>
2024-11-26 13:34:19 -0500

    Update script.js

--- loginRegister/views/public/js/script.js
+++ loginRegister/views/public/js/script.js
@@ -6,6 +6,7 @@ document.getElementById("btnFormRegister").addEventListener("click", function ()
 document.getElementById("btnFormLogin").addEventListener("click", function () {
     document.getElementById("login-usuario").style.display = "none";
     document.getElementById("register-usuario").style.display = "flex";
+    
 });
 
 
